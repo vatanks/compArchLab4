70895bc01486ab59dca6f545f1b121395258ab13
Jazmin Munoz <Horchata-Kun@users.noreply.github.com>
2018-12-13 19:36:09 -0700

    FINAL

--- teamatarivcs_cputest.v
+++ teamatarivcs_cputest.v
@@ -10,7 +10,7 @@
 // 	        	Date: 12/14/2018                                                        
 //************************************************************************************************************
 `timescale 1ns/100ps
-`include "student_cpu.v"
+`include "teamatarivcs_cpu.v"
 module cpu_tb;
 
     reg Reset;              // Reset signal
@@ -39,7 +39,7 @@ module cpu_tb;
     // Test bench
     initial
     begin
-        // Test 1: d = a + b - c
+        // Test 1a: d = a + b - c
         
         #1 Reset = 1;
         #1 Reset = 0;
@@ -73,7 +73,107 @@ module cpu_tb;
         #1 newInstr = 1;
         #1 newInstr = 0; 
         // Display
-        #1 $display("Test 1: 10 + 22 - 6");
+        #1 $display("Test 1a: 10 + 22 - 6");
+        #1 $display("a = %d", myCPU.myDataMem.memory[0]);
+        #1 $display("b = %d", myCPU.myDataMem.memory[1]);
+        #1 $display("c = %d", myCPU.myDataMem.memory[2]);
+        #1 $display("d = %d", myCPU.myDataMem.memory[3]);        
+        
+        #1checkreg = (myCPU.myDataMem.memory[0] + myCPU.myDataMem.memory[1] - myCPU.myDataMem.memory[2]);
+        // Checking if value is correct 
+        if (checkreg == myCPU.myDataMem.memory[3]) begin
+            $display("This output is correct : %d", checkreg);
+            checkreg = 0;
+        end
+        else $display("That output is not correct, this output is correct ---> %d", checkreg);
+
+        //*****************************************************
+		
+		        // Test 1b: d = a + b - c
+        
+        #1 Reset = 1;
+        #1 Reset = 0;
+        #1 myCPU.myDataMem.memory[0] = 5;
+        #1 myCPU.myDataMem.memory[1] = 11;
+        #1 myCPU.myDataMem.memory[2] = 3;
+        // lw $1, 0($0); 
+        #10 instrWord= 32'b100011_00000_00001_0000000000000000; 
+        #1 newInstr=1;        
+
+        #1 newInstr=0;
+
+        // lw $2, 1($0);
+        #10 instrWord= 32'b100011_00000_00010_0000000000000001; 
+        #1 newInstr=1;
+        #1 newInstr=0;
+        // lw $3, 2($0);
+        #10 instrWord= 32'b100011_00000_00011_0000000000000010; 
+        #1 newInstr=1;
+        #1 newInstr=0;
+        // add $4, $1, $2; a + b
+        #10 instrWord= 32'b000000_00001_00010_00100_00000_100000;
+        #1 newInstr=1;
+        #1 newInstr=0;
+        // sub $4, $4, $3; ( a + b ) - c
+        #10 instrWord= 32'b000000_00100_00011_00100_00000_100010;
+        #1 newInstr=1;
+        #1 newInstr=0;
+        // sw $4, 3($0)
+        #10 instrWord = 32'b101011_00000_00100_0000000000000011;
+        #1 newInstr = 1;
+        #1 newInstr = 0; 
+        // Display
+        #1 $display("Test 1b: 5 + 11 - 3");
+        #1 $display("a = %d", myCPU.myDataMem.memory[0]);
+        #1 $display("b = %d", myCPU.myDataMem.memory[1]);
+        #1 $display("c = %d", myCPU.myDataMem.memory[2]);
+        #1 $display("d = %d", myCPU.myDataMem.memory[3]);        
+        
+        #1checkreg = (myCPU.myDataMem.memory[0] + myCPU.myDataMem.memory[1] - myCPU.myDataMem.memory[2]);
+        // Checking if value is correct 
+        if (checkreg == myCPU.myDataMem.memory[3]) begin
+            $display("This output is correct : %d", checkreg);
+            checkreg = 0;
+        end
+        else $display("That output is not correct, this output is correct ---> %d", checkreg);
+
+        //*****************************************************
+		
+		        // Test 1c: d = a + b - c
+        
+        #1 Reset = 1;
+        #1 Reset = 0;
+        #1 myCPU.myDataMem.memory[0] = 2;
+        #1 myCPU.myDataMem.memory[1] = 20;
+        #1 myCPU.myDataMem.memory[2] = 7;
+        // lw $1, 0($0); 
+        #10 instrWord= 32'b100011_00000_00001_0000000000000000; 
+        #1 newInstr=1;        
+
+        #1 newInstr=0;
+
+        // lw $2, 1($0);
+        #10 instrWord= 32'b100011_00000_00010_0000000000000001; 
+        #1 newInstr=1;
+        #1 newInstr=0;
+        // lw $3, 2($0);
+        #10 instrWord= 32'b100011_00000_00011_0000000000000010; 
+        #1 newInstr=1;
+        #1 newInstr=0;
+        // add $4, $1, $2; a + b
+        #10 instrWord= 32'b000000_00001_00010_00100_00000_100000;
+        #1 newInstr=1;
+        #1 newInstr=0;
+        // sub $4, $4, $3; ( a + b ) - c
+        #10 instrWord= 32'b000000_00100_00011_00100_00000_100010;
+        #1 newInstr=1;
+        #1 newInstr=0;
+        // sw $4, 3($0)
+        #10 instrWord = 32'b101011_00000_00100_0000000000000011;
+        #1 newInstr = 1;
+        #1 newInstr = 0; 
+        // Display
+        #1 $display("Test 1c: 2 + 20 - 7");
         #1 $display("a = %d", myCPU.myDataMem.memory[0]);
         #1 $display("b = %d", myCPU.myDataMem.memory[1]);
         #1 $display("c = %d", myCPU.myDataMem.memory[2]);
